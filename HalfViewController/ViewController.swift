7fc543c06a2006029fbce39c859dff4d2e8b2f83
Swasidhant <swasidhant.chowdhury@paytm.com>
2020-12-08 16:43:35 +0530

    Initial Commit

---
+++ HalfViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HalfViewController
+//
+//  Created by swasidhant chowdhury on 08/12/20.
+//  Copyright © 2020 swasidhant chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
